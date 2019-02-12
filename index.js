d42a529b276f391f9d1c77847dfba9d6281542dc
Zach Latta <zach@zachlatta.com>
2019-02-11 23:09:54 -0800

    Add launch message

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 require('babel-register');
-require('./airtable.js');
+require('./main.js');
---
+++ main.js
@@ -0,0 +1,4 @@
+// Entrypoint of application in ES6
+console.log(`Airbender taking off...`)
+
+require('./airtable.js')
