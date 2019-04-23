73e90db0819ecfb93f76b63a562efca8819fca0c
Max Wofford <max@maxwofford.com>
2019-04-22 21:34:17 -0700

    Add form submission task

---
+++ bases/formSubmissions.js
@@ -0,0 +1,38 @@
+// The table we track submissions in can be found here: https://airtable.com/tblmzNkrQ4v5zEZus/viwIJwBpxkc9HpCPv?blocks=hide
+// The Zap that submits to Airtable can be found here: https://zapier.com/app/editor/56889892/overview
+
+const Airtable = require('airtable')
+
+const config = require('../config.js')
+const util = require('../util.js')
+
+const base = new Airtable({apiKey: config.airtable.apiKey}).base(config.airtable.bases.formSubmissions)
+
+module.exports = () => {
+  return util.forEachInTable(base, 'Submissions', submission => {
+    if (submission.get('Populate Fields') && !submission.get('Generated Record')) {
+      try {
+        const body = JSON.parse(submission.get('Body'))
+        const bodyTable = body.table
+        const bodyBase = body.base
+        const bodyRecord = body.record
+
+        const destination = new Airtable({apiKey: config.airtable.apiKey}).base(bodyBase)
+        return destination.table(bodyTable).create(bodyRecord, function(err, result) {
+          if (err) { return err.message }
+          return submission.patchUpdate({
+            'Table': bodyTable,
+            'Base': bodyBase,
+            'Populate Fields': null,
+            'Status': 'Success'
+          })
+        })
+      } catch (e) {
+        return submission.patchUpdate({
+          'Populate Fields': null,
+          'Status': e.message
+        })
+      }
+    }
+  })
+}
--- config.js
+++ config.js
@@ -3,7 +3,8 @@ module.exports = {
     apiKey: process.env.AIRTABLE_KEY,
     bases: {
       shipping: process.env.AIRTABLE_SHIPPING_BASE,
-      hardwareDonations: process.env.AIRTABLE_HARDWARE_DONATIONS_BASE
+      hardwareDonations: process.env.AIRTABLE_HARDWARE_DONATIONS_BASE,
+      formSubmissions: process.env.AIRTABLE_FORM_SUBMISSIONS_BASE
     }
   },
   shippoKey: process.env.SHIPPO_KEY
