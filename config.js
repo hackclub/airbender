3b22e05f002f5bd0a32fe80b619f8ff3c52889f7
Max Wofford <max@maxwofford.com>
2019-06-24 20:49:30 -0700

    Add waitlist confirmation sender

--- config.js
+++ config.js
@@ -2,10 +2,12 @@ module.exports = {
   airtable: {
     apiKey: process.env.AIRTABLE_KEY,
     bases: {
+      operations: process.env.AIRTABLE_OPERATIONS_BASE,
       shipping: process.env.AIRTABLE_SHIPPING_BASE,
       hardwareDonations: process.env.AIRTABLE_HARDWARE_DONATIONS_BASE,
       formSubmissions: process.env.AIRTABLE_FORM_SUBMISSIONS_BASE
     }
   },
+  zapierWaitlistWebhook: process.env.ZAPIER_WAITLIST_WEBHOOK,
   shippoKey: process.env.SHIPPO_KEY
 }
