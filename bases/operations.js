21f8e9bad79a28f8188ae1e9133a9f03bc50def7
Max Wofford <max@maxwofford.com>
2020-01-05 10:50:22 -0500

    Replace waitlist with privacy card creation

--- bases/operations.js
+++ bases/operations.js
@@ -3,50 +3,26 @@ const fetch = require('node-fetch')
 
 const config = require('../config.js')
 const util = require('../util.js')
+const Privacy = require('../lib/privacy')
 
 const base = new Airtable({apiKey: config.airtable.apiKey}).base(config.airtable.bases.operations)
+const privacy = new Privacy
 
-const sendConfirmationTo = email => (
-  fetch(config.zapierWaitlistWebhook, {
-    method: 'post',
-    body: JSON.stringify({ email }),
-    headers: { 'Content-Type': 'application/json' }
-  })
-)
-
-module.exports = () => {
-  const sentEmails = new Set([])
-
-  return util.forEachInTable(base, 'Waitlist', applicant => {
-    const email = applicant.get('Email')
-
-    if (sentEmails.has(email)) {
-      if (applicant.get('Send Confirmation')) {
-        console.log(`Skipping duplicate waitlist entry for ${email} & noting in Airtable`)
-        return applicant.patchUpdate({
-          'Send Confirmation': false,
-          'Notes': 'This is a duplicate waitlist entry– email confirmation not sent'
-        })
-      }
-    } else {
-      sentEmails.add(email)
-
-      if (applicant.get('Send Confirmation')) {
-        console.log(`Sending waitlist confirmation to ${email}...`)
-        return sendConfirmationTo(email).then(_ => {
-          console.log(`Sent confirmation to ${email}`)
-          return applicant.patchUpdate({
-            'Send Confirmation': false,
-            'Notes': 'Email confirmation sent by Zapier'
-          })
-        }).catch(e => {
-          console.log(e.message)
-          return applicant.patchUpdate({
-            'Send Confirmation': false,
-            'Notes': `Automatic sending failed with error: ${e.message}`
-          })
-        })
-      }
+async function processClubs() {
+  util.forEachInTable(base, 'Clubs', async club => {
+    if (club.get('Should have Privacy Card' && !club.get('Privacy Card'))) {
+      const card = await privacy.createCard()
+      const cardRecord = await base('Privacy Cards').create({
+        'Privacy Token': card.token,
+        'Club': [club.id],
+        'Spending limit (cents)': 0,
+      })
     }
   })
 }
+
+module.exports = () => (
+  Promise.all([
+    processClubs(),
+  ])
+)
